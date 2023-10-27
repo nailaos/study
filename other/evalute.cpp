bdf846e2938f7601b02f2518908f072673ec2bc6
lvjx <lvjx22@mails.tsinghua.edn.cn>
2023-10-27 19:25:42 +0800

    improve the algorithm and add check function

--- other/evalute.cpp
+++ other/evalute.cpp
@@ -5,13 +5,48 @@
 
 using namespace std;
 
+bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+bool isNumeric(char c) {
+    return isdigit(c) || c == '.';
+}
+
+bool isValidExpression(const string& expression) {
+    stack<char> parentheses;
+    bool lastWasOperator = true;
+
+    for (char c : expression) {
+        if (c == '(') {
+            parentheses.push(c);
+            lastWasOperator = true;
+        } else if (c == ')') {
+            if (parentheses.empty()) {
+                return false; // Unmatched closing parenthesis
+            }
+            parentheses.pop();
+            lastWasOperator = false;
+        } else if (isOperator(c)) {
+            if (lastWasOperator) {
+                return false; // Two operators in a row
+            }
+            lastWasOperator = true;
+        } else if (isNumeric(c)) {
+            lastWasOperator = false;
+        } else if (c != ' ') {
+            return false; // Invalid character
+        }
+    }
+
+    return parentheses.empty() && !lastWasOperator;
+}
+
 int precedence(char op) {
     if (op == '+' || op == '-') {
         return 1;
     } else if (op == '*' || op == '/') {
         return 2;
-    } else if (op == '^') {
-        return 3;
     }
     return 0;
 }
@@ -26,14 +61,17 @@ double applyOperator(double a, double b, char op) {
             return a * b;
         case '/':
             return a / b;
-        case '^':
-            return pow(a, b);
         default:
             return 0;
     }
 }
 
 double evaluateExpression(const string& expression) {
+    if (!isValidExpression(expression)) {
+        cout << "Invalid expression" << endl;
+        return 0.0;
+    }
+
     stack<double> values;
     stack<char> operators;
 
@@ -61,7 +99,7 @@ double evaluateExpression(const string& expression) {
                 values.push(applyOperator(a, b, op));
             }
             operators.pop();
-        } else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+        } else if (isOperator(c)) {
             while (!operators.empty() && precedence(c) <= precedence(operators.top())) {
                 char op = operators.top();
                 operators.pop();
@@ -89,10 +127,29 @@ double evaluateExpression(const string& expression) {
 }
 
 int main() {
-    string expression = "78*(2^5-8)";
-    cin>>expression;
-    double result = evaluateExpression(expression);
-    cout << "表达式结果：" << result << endl;
+    string expression;
+    while (true) {
+        getline(cin, expression);
+        if (expression == "q")
+            return 0;
+        string result;
+        for (char c : expression) {
+            if (c != ' ' && c != '\n') {
+                result += c;
+            }
+        }
+        expression = result;
+        cout << "表达式为：" << expression << endl;
+        if (isValidExpression(expression)) {
+            double result = evaluateExpression(expression);
+            if(isinf(result)) 
+              cout<<"Division by zero"<<endl;
+            else
+              cout << "表达式结果：" << result << endl;
+        } else {
+            cout << "Invalid expression" << endl;
+        }
+    }
+
     return 0;
 }
-
