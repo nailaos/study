682a700e2bb2e577bf8f4bb218b7c1dd6f0bd280
lvjx <lvjx22@mails.tsinghua.edn.cn>
2023-10-28 17:19:32 +0800

    重新了解movabsq, i32_min~u32_max use movl, else use movabsq

--- CSI/chapter3/while.c
+++ CSI/chapter3/while.c
@@ -1,17 +1,33 @@
 long _do(long n) {
-	long result = 4294967295;
-	do {
-		result *= n;
-		n--;
-	} while (n > 1);
-	return result;
+    long result = 4294967295;
+    do {
+        result *= n;
+        n--;
+    } while (n > 1);
+    return result;
 }
 
 long _while(long n) {
-	long result = 4294967296;
-	while (n > 1) {
-		result *= n;
-		n--;
-	}
-	return result;
+    long result = 4294967296;
+    while (n > 1) {
+        result *= n;
+        n--;
+    }
+    return result;
+}
+
+long _for(long n) {
+    long result = -2147483648;
+    for (; n > 1; n--) {
+        result *= n;
+    }
+    return result;
+}
+
+long _test(long n) {
+    long result = -2147483649;
+    for (; n > 1; n--) {
+        result *= n;
+    }
+    return result;
 }
--- CSI/chapter3/while.s
+++ CSI/chapter3/while.s
@@ -32,5 +32,39 @@ _while:
 	.cfi_endproc
 .LFE1:
 	.size	_while, .-_while
+	.globl	_for
+	.type	_for, @function
+_for:
+.LFB2:
+	.cfi_startproc
+	movq	$-2147483648, %rax
+	jmp	.L7
+.L8:
+	imulq	%rdi, %rax
+	subq	$1, %rdi
+.L7:
+	cmpq	$1, %rdi
+	jg	.L8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	_for, .-_for
+	.globl	_test
+	.type	_test, @function
+_test:
+.LFB3:
+	.cfi_startproc
+	movabsq	$-2147483649, %rax
+	jmp	.L10
+.L11:
+	imulq	%rdi, %rax
+	subq	$1, %rdi
+.L10:
+	cmpq	$1, %rdi
+	jg	.L11
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	_test, .-_test
 	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
 	.section	.note.GNU-stack,"",@progbits
