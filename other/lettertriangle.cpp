1184607f201c053e695369240eded340c71af1f3
lvjx <lvjx22@mails.tsinghua.edn.cn>
2023-10-29 16:10:35 +0800

    输出字母金字塔

--- other/lettertriangle.cpp
+++ other/lettertriangle.cpp
@@ -1,17 +1,23 @@
-#include <iomanip>
+#include <cmath>
 #include <iostream>
 using namespace std;
 
-void printSymbol(char c, int n) {
-    for (int i = 0; i < n; i++)
-        cout << c;
+void print_tower(char c, int n, int spa) {
+    if ((n == 1 && spa == -1) || n == -1 && (int)c == 'A' - 1)
+        return;
+    for (int i = 0; i < spa; i++)
+        cout << " ";
+    int start = 'A' - c;
+    for (int i = start; i <= -start; i++)
+        cout << (char)(c - abs(i));
     cout << endl;
+    print_tower(char(c + n), n, spa - n);
 }
 
-// 干什么   输出以该字母为中心的字符串
-void printLetter(char c, int num) {}
-
 int main() {
-    printSymbol('=', 11);
-    return 0;
+    char c;
+    cin >> c;
+    int spa = c - 'A';
+    print_tower('A', 1, spa);
+    print_tower(c - 1, -1, 1);
 }
