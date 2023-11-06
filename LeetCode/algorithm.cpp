e31e240ba1a7f92e2c3e677b06c376f0f73e297a
lvjx <lvjx22@mails.tsinghua.edn.cn>
2023-11-07 00:17:14 +0800

    22 ^_^

--- LeetCode/algorithm.cpp
+++ LeetCode/algorithm.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <queue>
 #include <stack>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -701,6 +702,29 @@ class Solution {
         }
         p->next = list1 == nullptr ? list2 : list1;
         return ans->next;
+    }
+
+    // NOTE: 22.括号生成
+    // NOTE: 回溯 + 去重
+    void backPairs(unordered_set<string> &ans, string &str, int n) {
+        if (!n) {
+            ans.insert(str);
+            return;
+        }
+        string tmp = str;
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            str = tmp.substr(0, i) + "()" + tmp.substr(i, len - i);
+            backPairs(ans, str, n - 1);
+            str = tmp;
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        unordered_set<string> ans;
+        string str = "()";
+        backPairs(ans, str, n - 1);
+        return vector<string>(ans.begin(), ans.end());
     }
 
     // 309.买卖股票的最佳时机含冷冻期
