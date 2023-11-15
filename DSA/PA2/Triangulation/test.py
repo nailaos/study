bf0c4da954575cff8e918804389ebd96dda8b9c1
fighting <lvjx22@mails.tsinghua.edu.cn>
2023-11-15 23:31:22 +0800

    draw

--- DSA/PA2/Triangulation/test.py
+++ DSA/PA2/Triangulation/test.py
@@ -1,36 +1,38 @@
 import matplotlib.pyplot as plt
 
+
 def plot_points_and_lines(n, points):
     # 解析输入
     x = [point[0] for point in points]
     y = [point[1] for point in points]
 
     # 画出点
-    plt.scatter(x, y, color='red')
+    plt.scatter(x, y, color="red")
 
     # 画出连接线
     for i in range(n - 1):
-        plt.plot([x[i], x[i+1]], [y[i], y[i+1]], color='blue')
+        plt.plot([x[i], x[i + 1]], [y[i], y[i + 1]], color="blue")
 
     # 首尾相连
-    plt.plot([x[n-1], x[0]], [y[n-1], y[0]], color='blue')
+    plt.plot([x[n - 1], x[0]], [y[n - 1], y[0]], color="blue")
 
     # 显示图形
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Connected Points')
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Connected Points")
     plt.grid(True)
     plt.show()
 
-# 输入点的数量
-n = int(input("请输入点的数量："))
 
-# 输入每个点的坐标
-points = []
-for i in range(n):
-    x = float(input(f"请输入第 {i+1} 个点的 x 坐标："))
-    y = float(input(f"请输入第 {i+1} 个点的 y 坐标："))
-    points.append((x, y))
+# 从文件中读取数据
+file_path = input("请输入包含点坐标的文件路径：")
+
+with open(file_path, "r") as file:
+    # 读取点的数量
+    n = int(file.readline())
+
+    # 读取每个点的坐标
+    points = [tuple(map(float, file.readline().split())) for _ in range(n)]
 
 # 画图
 plot_points_and_lines(n, points)
