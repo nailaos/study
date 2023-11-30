62d8cfeaf84b909ef3e568a43402c6025f89cf41
吕嘉鑫 <lvjx22@mails.tsinghua.edn.cn>
2023-11-30 21:19:12 +0800

    "kidd有了思路，接下来需要实现数据离散化 ^_^"

---
+++ DSA/PA3/Kidd/kidd.cpp
@@ -0,0 +1,100 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+struct Range {
+    int start;
+    int end;
+};
+
+int n, m;
+Range *ranges;
+long long int *seg;
+short *lazy;
+bool *h;
+int lenS;
+
+void init() {
+    scanf("%d %d", &n, &m);
+    ranges = new Range[m];
+    h = new bool[m];
+    int numMax = 0;
+    int numMin = 2147483647;
+    for (int i = 0; i < m; i++) {
+        char c;
+        int a, b;
+        scanf(" %c %d %d", &c, &a, &b);
+        numMax = max(numMax, b);
+        numMin = min(numMin, a);
+        ranges[i].start = a;
+        ranges[i].end = b;
+        h[i] = (c == 'H');
+    }
+    numMin--;
+    n = numMax - numMin;
+    for (int i = 0; i < m; i++) {
+        ranges[i].start -= numMin;
+        ranges[i].end -= numMin;
+    }
+    seg = new long long[4 * n + 1];
+    lazy = new short[4 * n + 1];
+}
+
+void dealLazy(int x, int start, int end) {
+    int l = 2 * x;
+    int r = 2 * x + 1;
+    int mid = (start + end) / 2;
+    seg[l] += lazy[x] * (mid - start + 1);
+    seg[r] += lazy[x] * (end - mid);
+    lazy[l] += lazy[x];
+    lazy[r] += lazy[x];
+    lazy[x] = 0;
+}
+
+void updateTree(int from, int to, int start, int end, int x) {
+    if (from <= start && end <= to) {
+        seg[x] += end - start + 1;
+        lazy[x]++;
+        return;
+    }
+    int mid = (start + end) / 2;
+    if (lazy[x] && start != end)
+        dealLazy(x, start, end);
+    if (from <= mid)
+        updateTree(from, to, start, mid, 2 * x);
+    if (mid < to)
+        updateTree(from, to, mid + 1, end, 2 * x + 1);
+    seg[x] = seg[2 * x] + seg[2 * x + 1];
+}
+
+long long int searchTree(int from, int to, int start, int end, int x) {
+    if (from <= start && end <= to)
+        return seg[x];
+    int mid = (start + end) / 2;
+    if (lazy[x] && start != end)
+        dealLazy(x, start, end);
+    long long sum = 0;
+    if (from <= mid)
+        sum += searchTree(from, to, start, mid, 2 * x);
+    if (mid < to)
+        sum += searchTree(from, to, mid + 1, end, 2 * x + 1);
+    return sum;
+}
+
+int main() {
+    init();
+    for (int i = 0; i < m; i++) {
+        int from = ranges[i].start;
+        int to = ranges[i].end;
+        if (h[i]) {
+            updateTree(from, to, 1, n, 1);
+        } else {
+            long long int ans = searchTree(from, to, 1, n, 1);
+            printf("%lld\n", ans);
+        }
+    }
+    delete[] ranges;
+    delete[] seg;
+    delete[] lazy;
+    delete[] h;
+}
--- DSA/PA3/Kidd/main.cpp
+++ DSA/PA3/Kidd/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
@@ -7,47 +8,128 @@ struct Range {
     int end;
 };
 
+struct SegmentTree {
+    long long int val;
+    Range range;
+    int lazy;
+    int len;
+};
+
 int n, m;
-Range *H;
-Range *Q;
-long long int *seg;
-bool *lazy;
-int lenH = 0;
-int lenQ = 0;
+Range *ranges;
+SegmentTree *seg;
+bool *h;
+int len = 0;
 
 void init() {
     scanf("%d %d", &n, &m);
-    H = new Range[m];
-    Q = new Range[m];
+    ranges = new Range[m];
+    h = new bool[m];
     int numMax = 0;
     int numMin = 2147483647;
     for (int i = 0; i < m; i++) {
         char c;
         int a, b;
-        scanf("%c %d %d", &c, &a, &b);
+        scanf(" %c %d %d", &c, &a, &b);
         numMax = max(numMax, b);
         numMin = min(numMin, a);
-        if (c == 'H') {
-            H[lenH].start = a;
-            H[lenH].end = b;
-            lenH++;
-        } else {
-            Q[lenQ].start = a;
-            Q[lenQ].end = b;
-            lenQ++;
-        }
+        ranges[len].start = a;
+        ranges[len].end = b;
+        h[len] = (c == 'H');
+        len++;
     }
-    n = numMax - numMin + 1;
-    seg = new long long[n];
-    lazy = new bool[n];
+    numMin--;
+    n = numMax - numMin;
+    for (int i = 0; i < len; i++) {
+        ranges[i].start -= numMin;
+        ranges[i].end -= numMin;
+    }
+    seg = new SegmentTree[4 * n];
 }
 
-void hand() {}
+void buildTree(int from, int to, int p) {
+    if (from == to) {
+        seg[p].range.start = from;
+        seg[p].range.end = to;
+        seg[p].len = 1;
+        return;
+    }
+    seg[p].range.start = from;
+    seg[p].range.end = to;
+    seg[p].len = to - from + 1;
+    int mid = (from + to) / 2;
+    buildTree(from, mid, 2 * p);
+    buildTree(mid + 1, to, 2 * p + 1);
+}
 
-void solve() {}
+void printTree() {
+    for (int i = 1; i <= 2 * n; i++) {
+        if (seg[i].range.start == 0)
+            break;
+        cout << setw(2) << i << " Range: [" << seg[i].range.start << ","
+             << seg[i].range.end << "]  Value: " << seg[i].val
+             << " Lazy: " << seg[i].lazy << " Len: " << seg[i].len << endl;
+    }
+}
+
+void dealLazy(int p, int l, int r) {
+    seg[l].val += seg[p].lazy * seg[l].len;
+    seg[r].val += seg[p].lazy * seg[r].len;
+    seg[l].lazy += seg[p].lazy;
+    seg[r].lazy += seg[p].lazy;
+    seg[p].lazy = 0;
+}
+
+void updateTree(int from, int to, int p) {
+    if (from <= seg[p].range.start && seg[p].range.end <= to) {
+        seg[p].val += seg[p].len;
+        seg[p].lazy++;
+        return;
+    }
+    int mid = (seg[p].range.start + seg[p].range.end) / 2;
+    int l = 2 * p;
+    int r = 2 * p + 1;
+    if (seg[p].lazy && seg[p].len > 1)
+        dealLazy(p, l, r);
+    if (from <= mid)
+        updateTree(from, to, l);
+    if (mid < to)
+        updateTree(from, to, r);
+    seg[p].val = seg[l].val + seg[r].val;
+}
+
+long long int rangeSearch(int from, int to, int p) {
+    if (from <= seg[p].range.start && seg[p].range.end <= to)
+        return seg[p].val;
+    int mid = (seg[p].range.start + seg[p].range.end) / 2;
+    int l = 2 * p;
+    int r = 2 * p + 1;
+    if (seg[p].lazy && seg[p].len > 1)
+        dealLazy(p, l, r);
+    long long sum = 0;
+    if (from <= mid)
+        sum += rangeSearch(from, to, l);
+    if (mid < to)
+        sum += rangeSearch(from, to, r);
+    return sum;
+}
 
 int main() {
     init();
-    hand();
-    solve();
+    buildTree(1, n, 1);
+    for (int i = 0; i < len; i++) {
+        int from = ranges[i].start;
+        int to = ranges[i].end;
+        if (h[i]) {
+            updateTree(from, to, 1);
+            // printTree();
+        } else {
+            long long int ans = rangeSearch(from, to, 1);
+            printf("%lld\n", ans);
+            // printTree();
+        }
+    }
+    delete[] ranges;
+    delete[] seg;
+    delete[] h;
 }
